7216efa84d8ea2e0a031edd14a3c5e4101d9365a
Jason Javier <jasonjjavier@gmail.com>
2025-03-11 22:22:29 -0300

    initial commit

---
+++ src/main/java/com/mycompany/carljayereso/Carljayereso.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.carljayereso;
+
+import com.login.LoginFrame;
+
+/**
+ *
+ * @author carljayereso
+ */
+public class Carljayereso {
+
+    public static void main(String[] args) {
+        LoginFrame loginObj = new LoginFrame();
+        loginObj.show();
+    }
+}
